7ca19e52298f2b96098fe16e12951e9cd72fa67e
CodeRohanCode <rpareek.2029@gmail.com>
2018-10-27 22:24:50 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+const nodemailer = require('nodemailer');
+var smtpTransport = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    auth: {
+        user: "jiomerelal.test",
+        pass: "Iw2bagpiml_"
+    }
+});
+
+var mailOptions={
+    to : "rohan.pareek@mindtree.com",
+    subject : "Node Mail",
+    text : "Hi, This mail is auto generated through Node.",
+    html : "<h1>And this is with <b>HTML</b><i>tags</i></h1>"
+}
+console.log(mailOptions);
+smtpTransport.sendMail(mailOptions, function(error, response){
+ if(error){
+        console.log(error);
+    res.end("error");
+ }else{
+        console.log("Message sent: " + response.message);
+    res.end("sent");
+     }
+});
